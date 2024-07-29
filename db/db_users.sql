4b1d3c04cb8950f8ac4d1748143f13f74fa94e76
shiddiq-21 <xiie.sidiq03@gmail.com>
2024-07-29 10:26:15 +0700

    Add files via upload

---
+++ db/db_users.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 29 Jul 2024 pada 05.21
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_users`
+--
+
+CREATE TABLE `tbl_users` (
+  `fullname` varchar(50) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `institution` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`fullname`, `username`, `institution`, `email`, `password`) VALUES
+('', '', '', '', ''),
+('Shiddiq', 'beyeelagii', 'asda', 'ashshiddiq1910@gmail.com', 'shiddiq21'),
+('ash shiddiq', '_ashiddiq', 'asda', 'ashshiddiq1910@gmail.com', '1');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
